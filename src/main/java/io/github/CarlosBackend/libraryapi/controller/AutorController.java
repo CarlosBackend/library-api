da3fef5b6a45de460097a3a660d8899d84b72bcb
Carlos Henrique <carlinhosbackend@gmail.com>
2025-05-20 21:03:10 -0300

    Add AutorDTO

--- src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
@@ -1,5 +1,11 @@
 package io.github.CarlosBackend.libraryapi.controller;
 
+import io.github.CarlosBackend.libraryapi.controller.dto.AutorDTO;
+import io.github.CarlosBackend.libraryapi.model.Autor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 //http://localhost:8080/autores
 public class AutorController {
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody AutorDTO autor){
+
+        return new ResponseEntity("Autor salvo com sucesso" + autor, HttpStatus.CREATED);
+    }
 }
