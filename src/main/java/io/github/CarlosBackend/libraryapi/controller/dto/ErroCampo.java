a21f07c9bdbb8db17684bd92a044afd852aa1568
Carlos Henrique <carlinhosbackend@gmail.com>
2025-05-23 18:34:08 -0300

    added features - get details and delete autor

---
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/dto/ErroCampo.java
@@ -0,0 +1,4 @@
+package io.github.CarlosBackend.libraryapi.controller.dto;
+
+public record ErroCampo(String campo, String erro) {
+}
--- src/main/java/io/github/CarlosBackend/libraryapi/service/AutorService.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import io.github.CarlosBackend.libraryapi.model.Autor;
 import io.github.CarlosBackend.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,10 +20,38 @@ public class AutorService {
     public Autor salvar(Autor autor){
         return autorRepository.save(autor);
     }
+    public void atualizar(Autor autor){
+        if(autor.getId() == null){
+            throw new IllegalArgumentException("Para atualizar, é necessário que o autor já esteja salvo na base de dados");
+        }
+        autorRepository.save(autor);
+    }
+    public List<Autor> listar(){
+        return autorRepository.findAll();
+    }
+    public long count(){
+        return autorRepository.count();
+    }
+
     public Optional<Autor> obterPorId(UUID id){
         return autorRepository.findById(id);
     }
+
     public void deletar(Autor autor){
         autorRepository.delete(autor);
     }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+        if(nome != null){
+            return autorRepository.findByNome(nome);
+        }
+        if(nacionalidade != null){
+            return autorRepository.findByNacionalidade(nacionalidade);
+        }
+        return autorRepository.findAll();
+    }
+
 }
