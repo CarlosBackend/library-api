a2ec5ec86be20aa2a5f723b5f98f3a69fbbe1a88
Carlos Henrique <carlinhosbackend@gmail.com>
2025-05-23 18:34:08 -0300

    added features - get details and delete autor

--- src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
@@ -1,16 +1,16 @@
 package io.github.CarlosBackend.libraryapi.controller;
-
 import io.github.CarlosBackend.libraryapi.controller.dto.AutorDTO;
 import io.github.CarlosBackend.libraryapi.model.Autor;
 import io.github.CarlosBackend.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -23,7 +23,6 @@ public class AutorController {
     public AutorController(AutorService service) {
         this.service = service;
     }
-
 
     @PostMapping
     public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
@@ -60,7 +59,37 @@ public class AutorController {
             if(autorOptional.isEmpty()){
                 return ResponseEntity.notFound().build();
             }
-            service.deletar(autorOptional.get());git s
+            service.deletar(autorOptional.get());
             return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(autor.getId(),
+                autor.getNome(),
+                autor.getDate(),
+                autor.getNacionalidade())
+        ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable("id") String id, @RequestBody AutorDTO dto){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Autor autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setDate(dto.dataNascimento());
+        autor.setNacionalidade(dto.nacionalidade());
+        service.atualizar(autor);
+        return ResponseEntity.noContent().build();
     }
 }
