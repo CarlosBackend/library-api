f791f376fa16661fef6fc9c57cc492c0719a308f
Carlos Henrique <carlinhosbackend@gmail.com>
2025-05-22 21:28:53 -0300

    add details tutor and delete

--- src/main/java/io/github/CarlosBackend/libraryapi/Application.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/Application.java
@@ -3,8 +3,11 @@ import io.github.CarlosBackend.libraryapi.model.Autor;
 import io.github.CarlosBackend.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import java.time.LocalDate;
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
@@ -5,13 +5,12 @@ import io.github.CarlosBackend.libraryapi.model.Autor;
 import io.github.CarlosBackend.libraryapi.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/autores")
@@ -34,8 +33,34 @@ public class AutorController {
         //http://localhost:8080/autores/kdfjnejsnfefbsjfeb
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(autorEntidade.getId()).toUri();
-
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+        if(autorOptional.isPresent()){
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(autor.getId(),
+                    autor.getNome(),
+                    autor.getDate(),
+                    autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id){
+            var idAutor = UUID.fromString(id);
+            Optional<Autor> autorOptional = service.obterPorId(idAutor);
+            if(autorOptional.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }
+            service.deletar(autorOptional.get());
+            return ResponseEntity.noContent().build();
     }
 }
