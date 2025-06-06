5529d93e42a144943121998c74a3612f6708e24e
Carlos Henrique <carlinhosbackend@gmail.com>
2025-06-06 18:09:42 -0300

    get and delete from livro added

--- src/main/java/io/github/CarlosBackend/libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package io.github.CarlosBackend.libraryapi.controller.common;
-
 import io.github.CarlosBackend.libraryapi.controller.dto.ErroCampo;
 import io.github.CarlosBackend.libraryapi.controller.dto.ErroResposta;
+import io.github.CarlosBackend.libraryapi.exceptions.OperacaoNaoPermitidaException;
+import io.github.CarlosBackend.libraryapi.exceptions.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,5 +23,25 @@ public class GlobalExceptionHandler {
                 .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
                 .collect(Collectors.toList());
         return new ErroResposta(HttpStatus.UNPROCESSABLE_ENTITY.value(),"Erro de validação", listaErros);
+    }
+
+    @ExceptionHandler(RegistroDuplicadoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErroResposta handleRegistroDuplicadoException(RegistroDuplicadoException e){
+        return ErroResposta.conflito(e.getMessage());
+    }
+
+    @ExceptionHandler(OperacaoNaoPermitidaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroResposta handleOperacaoNaoPermitida(OperacaoNaoPermitidaException e){
+        return ErroResposta.respostaPadrao(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErroResposta handleErrosNaoTratados(RuntimeException e){
+        return new ErroResposta(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro inesperado. Entre em contato com o administrador do sistema para resolver o problema. ",
+                List.of());
     }
 }
