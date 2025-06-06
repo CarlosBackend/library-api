d91f2fb57da8898e5d82b5609398d62f9f500463
Carlos Henrique <carlinhosbackend@gmail.com>
2025-06-06 18:09:42 -0300

    get and delete from livro added

--- src/main/java/io/github/CarlosBackend/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/mappers/LivroMapper.java
@@ -1,6 +1,7 @@
 package io.github.CarlosBackend.libraryapi.controller.mappers;
 
 import io.github.CarlosBackend.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.CarlosBackend.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.CarlosBackend.libraryapi.model.Livro;
 import io.github.CarlosBackend.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 
 // Se nao colocar o componentModel, o mapstruct nao vai funcionar
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {AutorMapper.class})
 public abstract class LivroMapper {
 
     @Autowired
@@ -17,4 +18,6 @@ public abstract class LivroMapper {
 
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
     public abstract Livro toEntity(CadastroLivroDTO dto);
+
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
