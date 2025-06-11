376e2adde0f0103aad4a5cb6efdf8198783ea50e
Carlos Henrique <carlinhosbackend@gmail.com>
2025-06-11 11:49:18 -0300

    new filter added

--- src/main/java/io/github/CarlosBackend/libraryapi/service/LivroService.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/service/LivroService.java
@@ -1,12 +1,17 @@
 package io.github.CarlosBackend.libraryapi.service;
-
+import io.github.CarlosBackend.libraryapi.model.GeneroLivro;
 import io.github.CarlosBackend.libraryapi.model.Livro;
 import io.github.CarlosBackend.libraryapi.repository.LivroRepository;
+import io.github.CarlosBackend.libraryapi.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.CarlosBackend.libraryapi.repository.specs.LivroSpecs.anoPublicacaoEqual;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +22,38 @@ public class LivroService {
         livroRepository.save(livro);
         return livro;
     }
-    public Optional<Livro> obterPorId(UUID id){
+
+    public Optional<Livro> obterPorId(UUID id) {
         return livroRepository.findById(id);
     }
-    public void deletar(Livro livro){
+
+    public void deletar(Livro livro) {
         livroRepository.delete(livro);
     }
-    public void atualizar(Livro livro){
+
+    public void atualizar(Livro livro) {
         livroRepository.save(livro);
+    }
+
+    // isbn, titulo, nome autor, genero, ano de publicacao
+    public List<Livro> pesquisa(String isbn, String titulo,String nomeAutor,
+                                GeneroLivro generoLivro, Integer anoPublicacao) {
+
+        // select * from livro where 0 = 0
+        Specification<Livro> specs = Specification.where((root, query, criteriaBuilder)
+                -> criteriaBuilder.conjunction());
+        if(isbn != null){
+            specs = specs.and(LivroSpecs.isbnsEqual(isbn));
+        }
+        if(titulo != null){
+            specs = specs.and(LivroSpecs.titulosLike(titulo));
+        }
+        if(generoLivro != null){
+            specs = specs.and(LivroSpecs.titulosLike(generoLivro.name()));
+        }
+        if(anoPublicacao != null){
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+        return livroRepository.findAll(specs);
     }
 }
