52b5af00a150d074a8ea29b61e4ae130e3eb29ed
Carlos Henrique <carlinhosbackend@gmail.com>
2025-07-10 20:41:41 -0300

    Validando ano e data de publicação dos livros

--- src/main/java/io/github/CarlosBackend/libraryapi/validador/LivroValidator.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/validador/LivroValidator.java
@@ -1,5 +1,7 @@
 package io.github.CarlosBackend.libraryapi.validador;
 
+import io.github.CarlosBackend.libraryapi.exceptions.CampoInvalidoException;
+import io.github.CarlosBackend.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.CarlosBackend.libraryapi.model.Livro;
 import io.github.CarlosBackend.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +12,24 @@ import java.util.Optional;
 @Component // Para que o spring reconheça o componente
 @RequiredArgsConstructor // Para que o lombok gere o construtor padrão
 public class LivroValidator {
+    private static final int ano_exigencias_preco = 2020;
     private final LivroRepository repository;
 
     public void validar (Livro livro){
         if (existeLivroComIsbn(livro)){
-            throw new IllegalArgumentException("Livro já cadastrado com o isbn informado");
+            throw new RegistroDuplicadoException("Livro já cadastrado com o isbn informado");
+        }
+
+        if (isPrecoObrigatorioNulo(livro)){
+            throw new CampoInvalidoException("preco","O preço do livro é obrigatório para os anos de 2020 ou superior");
         }
     }
+
+    private boolean isPrecoObrigatorioNulo(Livro livro) {
+        return livro.getPreco() == null &&
+                livro.getDataPublicacao().getYear()>= ano_exigencias_preco;
+    }
+
     private boolean existeLivroComIsbn(Livro livro) {
         Optional<Livro> livroEncontrado = repository.findByIsbn(livro.getIsbn());
         if(livro.getId() == null){
