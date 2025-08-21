a5ef11e7900496c57c9c38406ddc235056167c58
Carlos Henrique <carlinhosbackend@gmail.com>
2025-08-21 17:12:33 -0300

    feat: configurando o security

---
+++ src/main/java/io/github/CarlosBackend/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package io.github.CarlosBackend.libraryapi.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // desabilita o csrf
+                .formLogin(Customizer.withDefaults()) // habilita o form login
+                .httpBasic(Customizer.withDefaults()) // habilita o basic auth
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                }) // autoriza qualquer requisicao
+                .build();
+    }
+}
