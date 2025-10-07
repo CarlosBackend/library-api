2fdf174fdab39a9ab9e0fa7b64602ec56687fa88
Carlos Henrique <carlinhosbackend@gmail.com>
2025-10-07 00:35:30 -0300

    feat: ajustando as config do arquivo yml

--- src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/CarlosBackend/libraryapi/controller/AutorController.java
@@ -1,17 +1,13 @@
 package io.github.CarlosBackend.libraryapi.controller;
+
 import io.github.CarlosBackend.libraryapi.controller.dto.AutorDTO;
 import io.github.CarlosBackend.libraryapi.controller.mappers.AutorMapper;
 import io.github.CarlosBackend.libraryapi.model.Autor;
-import io.github.CarlosBackend.libraryapi.model.Usuario;
-import io.github.CarlosBackend.libraryapi.security.SecurityService;
 import io.github.CarlosBackend.libraryapi.service.AutorService;
-import io.github.CarlosBackend.libraryapi.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
